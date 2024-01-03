47d4d55f9d24272bd5720db09826080b6035c922
fengpipi <60679171+fengpipi@users.noreply.github.com>
2024-01-03 16:21:31 +0800

    added auth

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import logo from "./logo.svg";
 import "@aws-amplify/ui-react/styles.css";
+import { Amplify } from 'aws-amplify';
+import config from './aws-exports';
+Amplify.configure(config);
 import {
   withAuthenticator,
   Button,
